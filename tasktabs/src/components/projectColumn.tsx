14c5f707c6d503ee6a3ee2f0206dc92dd6472736
Woom <theroyalblah@gmail.com>
2020-03-21 01:59:13 -0400

    resize height of sidebars

--- tasktabs/src/components/projectColumn.tsx
+++ tasktabs/src/components/projectColumn.tsx
@@ -4,13 +4,17 @@ import styled from 'styled-components';
 import { SubTaskButton } from './subTaskButton';
 import { ProjectButton } from './newProjectButton';
 
+interface ColumnProps {
+    height: number;
+};
+
 const Column = styled.div`
   display: flex;
   flex-direction: column;
   border-width: 5px 5px 0px 5px;
   border-style: solid;
   width: 400px;
-  height: ${window.innerHeight}px;
+  height: ${(props: ColumnProps) => props.height}px;
 `;
 
 interface SubTask {
@@ -27,12 +31,37 @@ export class ProjectColumn extends React.Component<ProjectColumnProps> {
 
     constructor(props: ProjectColumnProps) {
         super(props);
+        
+        this.state = { height: 0 };
+    }
+
+    updateDimensions = () => {
+        this.setState({ height: window.innerHeight });
+        console.log(this.state);
+    };
+
+    // When this object is displayed, add an event that check for window resizes.
+    componentDidMount() {
+        window.addEventListener('resize', this.updateDimensions);
+    }
+
+    // Remove event when the object is unmounted.
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateDimensions);
+    }
+
+    // Make sure column doesn't get too small
+    checkHeight = () => {
+        const height = window.innerHeight;
+        return height > 825 ? height : 825;
     }
 
     render() {
+
+        const height = this.checkHeight();
         return (
             <>
-                <Column>
+                <Column height={height} >
                     <ProjectButton />
                     {this.props.subtasks.map((task) => {
                         return <SubTaskButton name={task.name} percentage={task.percentage} key={task.id}></SubTaskButton>;
--- tasktabs/src/components/subTaskColumn.tsx
+++ tasktabs/src/components/subTaskColumn.tsx
@@ -3,11 +3,16 @@ import styled from 'styled-components';
 
 import { SubTaskButton } from './subTaskButton';
 
+interface ColumnProps {
+    height: number;
+};
+
 const Column = styled.div`
   display: flex;
   flex-direction: column;
   border-width: 5px 5px 5px 0px;
   border-style: solid;
+  height: ${(props: ColumnProps) => props.height}px;
 `;
 
 const NewTaskButton = styled.button`
@@ -34,11 +39,35 @@ export class SubTaskColumn extends React.Component<SubTaskColumnProps>{
 
     constructor(props: SubTaskColumnProps) {
         super(props);
+
+        this.state = { height: 0 };
+    }
+
+    updateDimensions = () => {
+        this.setState({ height: window.innerHeight });
+        console.log(this.state);
+    };
+
+    // When this object is displayed, add an event that check for window resizes.
+    componentDidMount() {
+        window.addEventListener('resize', this.updateDimensions);
+    }
+
+    // Remove event when the object is unmounted.
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateDimensions);
+    }
+
+    // Make sure column doesn't get too small
+    checkHeight = () => {
+        const height = window.innerHeight;
+        return height > 825 ? height : 825;
     }
 
     render() {
+        const height = this.checkHeight();
         return (
-            <Column>
+            <Column height = {height}>
                 <NewTaskButton>+ New Task</NewTaskButton>
                 {this.props.subtasks.map((task) => {
                     return <SubTaskButton name={task.name} percentage={task.percentage} key={task.id}></SubTaskButton>;
