788df4b2b104cf703a653c0b350a45104ea50f6c
unknown <justinlagree@live.com>
2020-04-01 17:11:08 -0400

    setup prod environment and update scripts.

--- api/index.ts
+++ api/index.ts
@@ -15,8 +15,16 @@ const port: string = process.env.PORT || ApplicationConfig.api.port.toString();
 // connect to our local mongo db
 mongoose.Promise = global.Promise;
 mongoose.set('useCreateIndex', true);
-mongoose.connect(`mongodb://localhost:${port}/${ApplicationConfig.database.name}`,
+
+if(process.env.NODE_ENV === "development") {  
+    mongoose.connect(`mongodb://localhost:${port}/${ApplicationConfig.database.name}`,
     { useNewUrlParser: true, useUnifiedTopology: true });
+}  
+else {
+    mongoose.connect(`mongodb://root:tasktabs1@ds133086.mlab.com:33086/heroku_hn0kpx12`,
+    { useNewUrlParser: true, useUnifiedTopology: true });
+}
+
 
 // allow cross origin requests
 app.use(cors());
